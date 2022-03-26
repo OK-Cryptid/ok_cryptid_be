ae964ef02b920ae0e025eaaeb721c204482c0337
Mallory Vining <mallorygallery12@gmail.com>
2022-03-26 14:49:42 -0600

    moving things around to try appeasing heroku

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,6 @@ module OkCryptidBe
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.assets.initialize_on_precompile = false
   end
 end
+config.assets.initialize_on_precompile = false
