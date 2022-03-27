3014241e45f85a30e70f8be39ba067e87c588914
Mallory Vining <mallorygallery12@gmail.com>
2022-03-27 15:32:59 -0600

    Db: schema

--- db/migrate/20220327202122_add_trails_to_sightings.rb
+++
@@ -1,5 +0,0 @@
-class AddTrailsToSightings < ActiveRecord::Migration[5.2]
-  def change
-    add_column :sightings, :trail_links, :string, array: true
-  end
-end
--- db/migrate/20220327202407_remove_trails_from_sightings.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTrailsFromSightings < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :sightings, :trails, :string
-  end
-end
