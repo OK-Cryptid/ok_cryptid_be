27699e0e543c47022541e0f0463ea7c8b9b343ea
Mallory Vining <mallorygallery12@gmail.com>
2022-03-27 16:08:12 -0600

    sightings test

--- spec/factories/sightings.rb
+++ spec/factories/sightings.rb
@@ -5,6 +5,6 @@ FactoryBot.define do
     association :cryptid, factory: :cryptid
     location { Faker::Address.state }
     description { Faker::Lorem.sentence(word_count: 5) }
-    image { Faker::LoremFlickr.image(seach_terms: ['cryptid']) }
+    image { Faker::LoremFlickr.image(search_terms: ['cryptid']) }
   end
 end
