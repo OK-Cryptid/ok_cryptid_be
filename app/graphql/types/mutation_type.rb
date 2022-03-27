873600c31a727c6bfd02907552ec4d1757f627da
Eric Chon <echon92@gmail.com>
2022-03-27 13:55:22 -0600

    create cryptid mutation working in graphiql, testing still needed

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,16 @@
 module Types
   class MutationType < Types::BaseObject
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
+    field :create_cryptid, CryptidType, null: false, description: "An example field added by the generator" do
+      argument :name, String
+      argument :range, String
+      argument :danger_level, String
+      argument :image, String
+      argument :description, String
+    end
+
+    def create_cryptid(payload)
+      Cryptid.create(payload)
     end
   end
 end
