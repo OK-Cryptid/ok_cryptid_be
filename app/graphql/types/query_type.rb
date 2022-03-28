8cf71865981047cf831db4df34790ca663007e1a
Mallory Vining <mallorygallery12@gmail.com>
2022-03-28 16:28:05 -0600

    Add cryptid by id refactor

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,23 +4,11 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    # field :cryptid_by_id, resolver: Queries::CryptidById
+    field :cryptid_by_id, resolver: Queries::CryptidById
     # field :cryptid_by_name, resolver: Queries::CryptidByName
     # field :cryptid_by_range, resolver: Queries::CryptidByRange
     field :get_cryptids, resolver: Queries::GetCryptids
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    #
-    field :cryptid_by_id, CryptidType, "get Cryptid by ID" do
-      argument :id, ID
-    end
-
-    def cryptid_by_id(args)
-      Cryptid.find(args[:id])
-    end
 
     field :cryptid_by_name, CryptidType, "Cryptid by name" do
       argument :name, String
---
+++ spec/graphql/queries/cryptids/cryptid_by_id_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+module Queries
+  RSpec.describe CryptidById, type: :request do
+      describe '.resolve' do
+        it 'returns a cryptid by id' do
+        cryptid = create(:cryptid, id: '1')
+
+        result = OkCryptidBeSchema.execute(cryptid_by_id_query).as_json
+
+        expect(result['data']['cryptidById']['name']).to be_a String
+        expect(result['data']['cryptidById']["description"]).to be_a String
+        expect(result['data']['cryptidById']['dangerLevel']).to be_a String
+        expect(result['data']['cryptidById']['range']).to be_a String
+        expect(result['data']['cryptidById']['image']).to be_a String
+      end
+    end
+  end
+end
+
+def cryptid_by_id_query
+  <<~GQL
+    {
+      cryptidById(id: "1") {
+        name
+        description
+        dangerLevel
+        range
+        image
+        }
+    }
+  GQL
+end
