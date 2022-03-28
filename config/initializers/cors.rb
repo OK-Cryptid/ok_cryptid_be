aef9b07634e92f25e7ea08aa72155e242fa3904d
Darren Kulback <dkulback@gmail.com>
2022-03-28 11:18:06 -0600

    test cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://ok-cryptid.herokuapp.com/'
+    origins 'http://ok-cryptid.herokuapp.com'
 
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
