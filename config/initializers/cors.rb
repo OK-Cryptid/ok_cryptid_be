3490b3d32cfbc531ae075f6a466eb9b871f56712
Mallory Vining <mallorygallery12@gmail.com>
2022-03-28 11:33:13 -0600

    no ssss

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://ok-cryptid.herokuapp.com'
+    origins 'http://ok-cryptid.herokuapp.com'
 
     resource '*',
       headers: :any,
