947e39c4a30c35ab48404aa1e99e4ae407adbdf7
Eric Chon <echon92@gmail.com>
2022-03-29 16:39:49 -0600

    search with name returns a collection

--- app/models/cryptid.rb
+++ app/models/cryptid.rb
@@ -2,9 +2,9 @@ class Cryptid < ApplicationRecord
   has_many :sightings
   validates :name, :range, :danger_level, :image, :description, presence: true
 
-  def self.search(search_params)
+  def self.search_name(search_params)
     if search_params.blank?
-      []
+      [Cryptid.find_by(name: "#{search_params}")]
     else
       where("name ILIKE ?", "%#{search_params}%")
     end
