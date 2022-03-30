b4aeeb8a94f47fa9e0181bfffcb82145d41d1cd4
Eric Chon <echon92@gmail.com>
2022-03-30 16:09:37 -0600

    Model tresting for search by name and range completed happy/sad/edgecase

--- spec/models/cryptid_spec.rb
+++ spec/models/cryptid_spec.rb
@@ -12,4 +12,46 @@ RSpec.describe Cryptid, type: :model do
     it { should validate_presence_of(:image) }
     it { should validate_presence_of(:description) }
   end
+
+  describe 'search_name method' do
+    it "returns all cryptids that match the key word" do
+      cryptid_1 = create(:cryptid, name: 'big')
+      cryptid_2 = create(:cryptid, name: 'Bigger')
+      result = Cryptid.search_name('big')
+      expect(result).to eq([cryptid_1, cryptid_2])
+    end
+
+    it "returns empty array if no match for keyword" do
+      cryptid_1 = create(:cryptid, name: 'big')
+      result = Cryptid.search_name('hello')
+      expect(result).to eq([])
+    end
+
+    it "returns empty array if keyword is blank" do
+      cryptid_1 = create(:cryptid, name: 'big')
+      result = Cryptid.search_name('')
+      expect(result).to eq([])
+    end
+  end
+
+  describe 'search_range method' do
+    it "returns all cryptids that match the keyword" do
+      cryptid_1 = create(:cryptid, range: 'California')
+      cryptid_2 = create(:cryptid, range: 'Virginia')
+      result = Cryptid.search_range('ia')
+      expect(result).to eq([cryptid_1, cryptid_2])
+    end
+
+    it "returns empty array if no match for keyword" do
+      cryptid_1 = create(:cryptid, range: 'Colorado')
+      result = Cryptid.search_range('hello')
+      expect(result).to eq([])
+    end
+
+    it "returns empty array if keyword is blank" do
+      cryptid_1 = create(:cryptid, range: 'Colorado')
+      result = Cryptid.search_range('')
+      expect(result).to eq([])
+    end
+  end
 end
