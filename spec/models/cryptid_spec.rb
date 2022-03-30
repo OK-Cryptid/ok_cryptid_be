897d5aefc238dd3db10a34cfc60dc3102775956c
Eric Chon <echon92@gmail.com>
2022-03-30 16:11:55 -0600

    cahnged double quotes to single for rubocop

--- spec/models/cryptid_spec.rb
+++ spec/models/cryptid_spec.rb
@@ -14,20 +14,20 @@ RSpec.describe Cryptid, type: :model do
   end
 
   describe 'search_name method' do
-    it "returns all cryptids that match the key word" do
+    it 'returns all cryptids that match the key word' do
       cryptid_1 = create(:cryptid, name: 'big')
       cryptid_2 = create(:cryptid, name: 'Bigger')
       result = Cryptid.search_name('big')
       expect(result).to eq([cryptid_1, cryptid_2])
     end
 
-    it "returns empty array if no match for keyword" do
+    it 'returns empty array if no match for keyword' do
       cryptid_1 = create(:cryptid, name: 'big')
       result = Cryptid.search_name('hello')
       expect(result).to eq([])
     end
 
-    it "returns empty array if keyword is blank" do
+    it 'returns empty array if keyword is blank' do
       cryptid_1 = create(:cryptid, name: 'big')
       result = Cryptid.search_name('')
       expect(result).to eq([])
@@ -35,20 +35,20 @@ RSpec.describe Cryptid, type: :model do
   end
 
   describe 'search_range method' do
-    it "returns all cryptids that match the keyword" do
+    it 'returns all cryptids that match the keyword' do
       cryptid_1 = create(:cryptid, range: 'California')
       cryptid_2 = create(:cryptid, range: 'Virginia')
       result = Cryptid.search_range('ia')
       expect(result).to eq([cryptid_1, cryptid_2])
     end
 
-    it "returns empty array if no match for keyword" do
+    it 'returns empty array if no match for keyword' do
       cryptid_1 = create(:cryptid, range: 'Colorado')
       result = Cryptid.search_range('hello')
       expect(result).to eq([])
     end
 
-    it "returns empty array if keyword is blank" do
+    it 'returns empty array if keyword is blank' do
       cryptid_1 = create(:cryptid, range: 'Colorado')
       result = Cryptid.search_range('')
       expect(result).to eq([])
